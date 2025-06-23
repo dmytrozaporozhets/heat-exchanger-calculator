8638a54bcf147729ef400448051b57032f1efda3
Dmytro <dmytrozaporozhets.kh@gmail.com>
2025-06-23 19:00:09 +0300

    Initial commit: create React + TypeScript app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Heat Exchanger Calculator</h1>
+      <p>Start building your engineering calculator here.</p>
+    </div>
+  );
+};
+
+export default App;
